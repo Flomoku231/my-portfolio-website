706ba8ad46811a7d935dd3c9d5a553fed8a46741
Flomoku231 <flomokuemmanuel114@gmail.com>
2022-12-09 15:02:13 +0200

    progress

---
+++ index.js
@@ -0,0 +1,16 @@
+// Coding skills progress bar
+// var a = document.getElementsByTagName('meter');
+// ax= 0;
+// ay=0;
+// az=0;
+
+// window.onload=()=>{
+//     const x = setInterval(()=>{
+//         ax +=1;
+//         a[0].value = ax;
+//         a[0].previousElementSibling.firstElementChild.innerHTML = ax + '%';
+//         if(ax >=10){
+//             clearInterval(x);
+//         }
+//     }, 10)
+// }
