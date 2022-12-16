360a3fbe073ba4dddb76315f03eed2de0c8f70d4
Flomoku231 <flomokuemmanuel114@gmail.com>
2022-12-16 10:36:49 +0200

    Complete HTML & CSS

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-// Coding skills progress bar
-// var a = document.getElementsByTagName('meter');
-// ax= 0;
-// ay=0;
-// az=0;
 
-// window.onload=()=>{
-//     const x = setInterval(()=>{
-//         ax +=1;
-//         a[0].value = ax;
-//         a[0].previousElementSibling.firstElementChild.innerHTML = ax + '%';
-//         if(ax >=10){
-//             clearInterval(x);
-//         }
-//     }, 10)
-// }
+// Popup logic
+var popup = document.querySelector ('.menu__popup--hide');
+
+
+function showMe(){
+   popup.classList.add("menu__popup--show");
+ };
+ function hideMe(){
+    popup.classList.remove("menu__popup--show");
+ };
